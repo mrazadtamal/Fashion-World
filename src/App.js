0e54ba13fc58631386dcb014c96f867c7b7655ae
mrazadtamal <mrazadt@gmail.com>
2022-02-02 21:36:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>azad</h1>
-    </div>
-  );
-}
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
