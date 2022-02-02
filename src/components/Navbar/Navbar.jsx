159e2c5026a00a5728bf69241e6a26f4dc9bef44
mrazadtamal <mrazadt@gmail.com>
2022-02-02 22:35:31 +0600

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">azad</div>
+  </nav>
 );
 
 export default Navbar;
