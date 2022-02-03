b201b42a9427d133b288e353f4a562b298fc917e
mrazadtamal <mrazadt@gmail.com>
2022-02-03 12:09:16 +0600

    li

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 import "./Navbar.css";
 
 const Navbar = () => (
   <nav className="app__navbar">
-    <div className="app__navbar-logo">azad</div>
+    <div className="app__navbar-logo">
+      <img
+        src={
+          "https://res.cloudinary.com/abidazad/image/upload/v1633197929/LOGO_1_d3bfdm.svg"
+        }
+        alt=""
+      />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
   </nav>
 );
 
